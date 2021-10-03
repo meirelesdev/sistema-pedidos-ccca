c1b5a70d84f6960bcd451390381dabf2ee8277b4
Daniel Meireles <daniel@inovadora.com.br>
2021-10-03 19:09:04 -0300

    finalizando a adição de uma segunda lib para rotas

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import ExpressHttp from "./infra/http/ExpressHttp";
+import HapiHttp from "./infra/http/HapiHttp";
 import RoutesConfig from "./infra/http/RoutesConfig";
 
 const http = new ExpressHttp();
+// const http = new HapiHttp()
 const routesConfig = new RoutesConfig(http)
 routesConfig.build()
-
-http.listen(3000)
+http.listen(3001)
--- test/integration/Http.test.ts
+++ test/integration/Http.test.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 test("Deve invocar a API /orders/${code}", async function(){
     const response = await axios({
-        url: "http://127.0.0.1:3000/orders/202100000001",
+        url: "http://127.0.0.1:3001/orders/202100000001",
         method: "get"
     })
     const order = response.data
